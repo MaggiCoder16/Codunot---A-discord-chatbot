cbe3e994618e9cac6953ff9cfee752250ef424c8
maggicoder <156202777+MaggiCoder16@users.noreply.github.com>
2026-01-14 10:34:44 +0530

    Update groq_bot.py

--- groq_bot.py
+++ groq_bot.py
@@ -64,7 +64,7 @@ rate_buckets = {}
 
 # ---------------- MODELS ----------------
 SCOUT_MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"  # seriousmode
-VERSATILE_MODEL = "meta-llama/llama-4-maverick-17b-128e-instruct"  # fun/roast
+VERSATILE_MODEL = "meta-llama/llama-4-scout-17b-16e-instruct"  # fun/roast
 
 SCOUT_COOLDOWN_UNTIL = None
 SCOUT_COOLDOWN_DURATION = timedelta(hours=1)
